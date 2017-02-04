b3ddfa27175a8fd200bc30af7b41e96d4e285845
Rakesh Gupta <grakesh18@gmail.com>
2017-02-04 17:01:47 +0530

    mocha test pass

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var should = require("should");
 
 // This agent refers to PORT where the program is running.
 
-var server = supertest.agent("http://localhost:4000");
+var server = supertest.agent("http://192.168.0.2:4000");
 
 // UNIT test begin
 
