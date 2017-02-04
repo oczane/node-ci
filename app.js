f3aef08d0a50dc95d52301b54ed9731bf9b1298b
Rakesh Gupta <grakesh18@gmail.com>
2017-02-04 14:37:22 +0530

    display text changes -1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("CI using Jenkins. IT automation using Ansible, Vagrant & VirtualVM - TEST - 2");
+  res.send("CI using Jenkins. IT automation using Ansible, Vagrant & VirtualVM - TEST - 002");
 });
 
 app.listen(4000);
